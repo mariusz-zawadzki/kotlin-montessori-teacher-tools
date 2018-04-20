5298a4ec9070aa820504472ba5ed0c287b6aee58
Mariusz Zawadzki <mariusz.r.zawadzki@gmail.com>
2018-04-21 01:06:43 +0200

    add some kotlin specific attributes

--- src/app/App.kt
+++ src/app/App.kt
@@ -1,22 +1,35 @@
 package app
 
-import react.*
-import react.dom.*
-import logo.*
-import ticker.*
+import kotlinx.html.classes
+import kotlinx.html.style
+import logo.logo
+import react.RBuilder
+import react.RComponent
+import react.RProps
+import react.RState
+import react.dom.code
+import react.dom.div
+import react.dom.h2
+import react.dom.p
+import ticker.ticker
 
 class App : RComponent<RProps, RState>() {
     override fun RBuilder.render() {
         div("App-header") {
             logo()
             h2 {
+                attrs.classes = setOf("Test","brzydkiAtrybut")
+                attrs["class"] = "Innyatrybut 123" +attrs.classes.joinToString(separator = " ")
+//                attrs.style = kotlinext.js.js {
+//                    display = "flex"
+//                }
                 +"Welcome to React with Kotlin"
                 +"Test thing"
             }
         }
         p("App-intro") {
             +"To get started, edit "
-            code { +"app/App.kt" }
+            code { +"src/app/App.kt" }
             +" and save to reload."
         }
         p("App-ticker") {
